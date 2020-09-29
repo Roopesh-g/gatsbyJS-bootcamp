05095422c86b7f2dec01d2f61d3efee41c5ad843
Roopesh-g <gujarathi.roopesh@gmail.com>
2020-09-29 13:14:22 +0100

    contact.js linked to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const IndexPage = () => {
   return (
     <div>
       <h1>Hello,</h1>
       <h2>I'm Roopesh, a full stack developer living in Ireland.</h2>
+      <p>Working with multiple pages...</p>
+      <p>Need a Developer? <a href="/contact">Contact Me</a> "this is using anchor html element"</p>
+      <p>Need a Developer? <Link to="/contact">Contact Me.</Link>"this is using the Link component. this component pre-loads the the pages the user might me heading to."</p>
     </div>
   )
 }
